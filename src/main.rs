b6011ab070f3b307f3e5911d20d4d274cd3a1bfd
Yeshwanth M <yeshwanth.m@seventablets.com>
2022-08-20 17:42:44 -0500

    Added colors to cli output

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use colored::*;
 use std::io;
 
 fn user_input() -> usize {
-    let err_msg = "Invalid input. Please enter a number between 1 and 7.";
+    let err_msg = "Invalid input. Please enter a number between 1 and 7.".bright_red();
     let player_input;
     loop {
         let mut input = String::new();
@@ -31,25 +32,36 @@ fn user_input() -> usize {
 
 fn print_output(output: &Vec<Vec<String>>) {
     output.iter().for_each(|it| {
-        println!("{:?}", it);
+        let merged: String = it.iter().map(|x| x.to_owned()).collect::<String>();
+        println!("{}", merged);
     });
+    println!("{}", "\n");
 }
 
-fn each_round(output: &mut Vec<Vec<String>>, each_column_count: &mut Vec<usize>) -> (usize, usize) {
-    println!("1st player, input a number between 1 to 7");
+fn each_round(
+    output: &mut Vec<Vec<String>>,
+    each_column_position: &mut Vec<usize>,
+) -> (usize, usize) {
+    println!(
+        "{}",
+        "1st player, input a number between 1 to 7".bright_cyan()
+    );
     let first_input = user_input();
 
     let first_loc = first_input - 1;
-    each_column_count[first_loc] = each_column_count[first_loc] - 1;
-    output[each_column_count[first_loc] + 1][first_loc] = String::from("[x]");
+    each_column_position[first_loc] -= 1;
+    output[each_column_position[first_loc] + 1][first_loc] = format!("{}", "[x]".bright_cyan());
 
     print_output(output);
 
-    println!("2nd player, input a number between 1 to 7");
+    println!(
+        "{}",
+        "2nd player, input a number between 1 to 7".bright_yellow()
+    );
     let second_input = user_input();
     let second_loc = second_input - 1;
-    each_column_count[second_loc] = each_column_count[second_loc] - 1;
-    output[each_column_count[second_loc] + 1][second_loc] = String::from("[O]");
+    each_column_position[second_loc] -= 1;
+    output[each_column_position[second_loc] + 1][second_loc] = format!("{}", "[0]".bright_yellow());
 
     print_output(output);
     // println!("1st Player: {}, 2nd Player: {}", first_input, second_input);
@@ -57,15 +69,14 @@ fn each_round(output: &mut Vec<Vec<String>>, each_column_count: &mut Vec<usize>)
 }
 
 fn main() {
-    println!("Hello, Welcome to Connect 4!");
-    println!("Need two players to play the game");
-
-    println!("Let's start!");
+    println!("{}", "\nHello, Welcome to Connect 4!".bright_cyan());
+    println!("{}", "Need 2 players to play the game".bright_purple());
+    println!("{}", "Let's start!\n".bright_green());
 
     let mut output = vec![vec![String::from("[ ]"); 7]; 7];
     print_output(&output);
-    let mut each_column_count = vec![6 as usize; 7];
+    let mut each_column_position = vec![6 as usize; 7];
     loop {
-        let (_first, _second) = each_round(&mut output, &mut each_column_count);
+        let (_first, _second) = each_round(&mut output, &mut each_column_position);
     }
 }
