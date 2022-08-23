18f99984e75d3ab61b031926e2e4e7e536058c93
Yeshwanth M <yeshwanth.m@seventablets.com>
2022-08-22 22:42:21 -0500

    Updated vec to hashmap for player name input logic

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
+use std::collections::HashMap;
+
 use colored::*;
 mod my_io;
 use my_io::game::Game;
 
 fn start_game(player_count: usize) {
-    let mut player_names = vec![String::from(""); player_count];
+    let mut player_names: HashMap<usize, String> = HashMap::new();
     for each in 1..=player_count {
-        player_names[each - 1] = my_io::inputs::get_player_names(&each);
+        player_names.insert(each, my_io::inputs::get_player_names(&each));
     }
     println!("{}", "Let's start!\n".bright_green());
     let mut game = Game::new();
     game.print_output();
     'outer: loop {
-        for each in 0..player_count {
-            let player = each as u8 + 1;
-            game.player_round(player, &player_names[each]);
+        for each in 1..=player_count {
+            let player = each as u8;
+            game.player_round(player, &player_names.get(&each).unwrap());
             if game.has_found_winner() {
                 break 'outer;
             }
