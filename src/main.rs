f548a80a104193e4c56185fc11d9c849f02133fb
Yeshwanth M <yeshwanth.m@seventablets.com>
2022-08-24 22:58:56 -0500

    Yew setup WIP

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,47 @@ use colored::Colorize;
 mod my_io;
 use my_io::game::Game;
 
+use yew::prelude::*;
+
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
 fn start_game(player_count: usize) {
     let mut player_names: HashMap<usize, String> = HashMap::new();
     for each in 1..=player_count {
@@ -26,6 +67,7 @@ fn start_game(player_count: usize) {
 }
 
 fn main() {
+    yew::start_app::<Model>();
     println!("{}", "\nHello, Welcome to Connect 4!".bright_cyan());
     let player_count = my_io::inputs::get_player_count();
     start_game(player_count);
